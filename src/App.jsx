e3a7ecbf26f133bac33e4b88dfe87122c7ae94a7
OhmPatil <workpatil10@gmail.com>
2024-06-04 16:16:24 +0530

    - Create card component. - Update tailwind theme config - Add relevant assets. - Create .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import Card from "./components/card/card.jsx"
-import './App.css'
-import cardItems from './constants/card.jsx'
+import { useState } from "react";
+import Card from "./components/card/card.jsx";
+import "./App.css";
+import cardItems from "./constants/card.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Card />
+      <section className="w-full bg-[#151329]">
+        <div className="w-fit grid grid-cols-2 md:grid-cols-3 gap-2 place-items-center p-2 mx-auto">
+          {cardItems.map((item, index) => {
+            return (
+              <Card
+                imageURL={item.imageURL}
+                publishedDate={item.publishedDate}
+                collectors={item.collectors}
+                collectionName={item.collectionName}
+                creatorName={item.creatorName}
+                showAvatars
+              />
+            );
+          })}
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'card-tag': 'radial-gradient(circle at 50% 50%, #7873D2 8%, #EFB7D3 35%, #A998D9 63%, #BEB6E5 100%);',
+      }
+    },
   },
   plugins: [],
 };
